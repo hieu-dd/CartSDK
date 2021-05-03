da6c5d0eb7e3e97fbee31b49986ba5a2e3aa0f03
acerPrise3 <ducthuan232tn@gmail.com>
2021-05-03 21:44:25 +0700

    first comit

--- app/src/main/java/com/cafeinlove14h/cartwithcompose/ui/screen/productlist/ProductListComponents.kt
+++ app/src/main/java/com/cafeinlove14h/cartwithcompose/ui/screen/productlist/ProductListComponents.kt
@@ -1,59 +1,116 @@
 package com.cafeinlove14h.cartwithcompose.ui.screen.productlist
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import com.cafeinlove14h.cartwithcompose.model.Product
+import com.cafeinlove14h.cartwithcompose.model.ProductInfo
 import com.google.accompanist.coil.CoilImage
 
 @Composable
-fun ProductItem(product: Product, onAddItem: (sku: String) -> Unit) {
+fun ProductListItem(product: Product) {
     ConstraintLayout(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(4.dp)
-            .background(Color.White)
-            .padding(top = 12.dp)
-            .clickable { onAddItem(product.productInfo.sku) }
-    ) {
-        val (image, tvName, tvSku) = createRefs()
+            .padding(2.dp)
+            .background(color = Color.White)
+            .padding(10.dp)
 
+    ) {
+        val (imageRef, numOfColorRef, color1Ref, color2Ref, color3Ref, nameRef, addProductRef, priceRef) = createRefs()
         CoilImage(
-            data = product.productInfo.imageUrl.orEmpty(),
-            contentDescription = product.productInfo.name,
+            data = product.productInfo.imageUrl ?: "",
+            contentDescription = "",
             modifier = Modifier
-                .width(100.dp)
-                .height(100.dp)
-                .constrainAs(image) {
+                .constrainAs(imageRef) {
                     top.linkTo(parent.top)
                     start.linkTo(parent.start)
-                },
-            contentScale = ContentScale.Crop
-        )
-        Text(
-            text = product.productInfo.name, modifier = Modifier
-                .constrainAs(tvName) {
-                    start.linkTo(image.end, margin = 10.dp)
-                    top.linkTo(image.top)
                     end.linkTo(parent.end)
-                    width = Dimension.preferredWrapContent
+                    width = Dimension.fillToConstraints
+                    height = Dimension.value(170.dp)
                 }
         )
-        Text(text = product.productInfo.sku, modifier = Modifier.constrainAs(tvSku) {
-            start.linkTo(tvName.start)
-            top.linkTo(tvName.bottom)
-            width = Dimension.preferredWrapContent
+        Text(text = "3 MÀU", modifier = Modifier
+            .constrainAs(numOfColorRef) {
+                top.linkTo(imageRef.bottom, margin = 5.dp)
+                start.linkTo(parent.start)
+            })
+        Circle(color = Color(0xFFE91E63), modifier = Modifier.constrainAs(color1Ref) {
+            top.linkTo(numOfColorRef.top)
+            bottom.linkTo(numOfColorRef.bottom)
+            start.linkTo(numOfColorRef.end, margin = 5.dp)
         })
+        Circle(color = Color(0xFF2196F3), modifier = Modifier.constrainAs(color2Ref) {
+            top.linkTo(numOfColorRef.top)
+            bottom.linkTo(numOfColorRef.bottom)
+            start.linkTo(color1Ref.end, margin = 5.dp)
+        })
+        Circle(color = Color(0xFFFFEB3B), modifier = Modifier.constrainAs(color3Ref) {
+            top.linkTo(numOfColorRef.top)
+            bottom.linkTo(numOfColorRef.bottom)
+            start.linkTo(color2Ref.end, margin = 5.dp)
+        })
+        Text(
+            text = product.productInfo.name,
+            color = Color(0xFFC96D50),
+            modifier = Modifier.constrainAs(nameRef) {
+                top.linkTo(numOfColorRef.bottom, margin = 5.dp)
+                start.linkTo(parent.start)
+            })
+        Text(
+            text = "${product.prices[0].sellPrice} vnđ",
+            color = Color(0xFFBB7A7A),
+            modifier = Modifier.constrainAs(priceRef) {
+                top.linkTo(nameRef.bottom, margin = 5.dp)
+                start.linkTo(parent.start, margin = 5.dp)
+            },
+            textDecoration = TextDecoration.LineThrough
+        )
+
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.White
+            ),
+            modifier = Modifier
+                .clip(shape = RoundedCornerShape(3.dp))
+                .background(color = Color(0xFFE60E0E))
+                .padding(2.dp)
+                .clip(shape = RoundedCornerShape(2.dp))
+                .constrainAs(addProductRef) {
+                    top.linkTo(priceRef.bottom, margin = 5.dp)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    width = Dimension.fillToConstraints
+                },
+        ) {
+            Text(text = "thêm vào giỏ")
+        }
     }
+}
+
+@Composable
+fun Circle(color: Color, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .width(10.dp)
+            .height(10.dp)
+            .clip(shape = RoundedCornerShape(5.dp))
+            .background(color)
+
+    )
 }
