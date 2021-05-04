cd710e0c73d35d7a1c07297663f4ddf3602d259c
acerPrise3 <ducthuan232tn@gmail.com>
2021-05-04 16:42:31 +0700

    second comit

--- app/src/main/java/com/cafeinlove14h/cartwithcompose/ui/screen/productlist/ProductListComponents.kt
+++ app/src/main/java/com/cafeinlove14h/cartwithcompose/ui/screen/productlist/ProductListComponents.kt
@@ -15,7 +15,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import com.cafeinlove14h.cartwithcompose.model.Product
@@ -26,12 +29,14 @@ import com.google.accompanist.coil.CoilImage
 fun ProductListItem(product: Product) {
     ConstraintLayout(
         modifier = Modifier
+            .clip(shape = RoundedCornerShape(3.dp))
             .padding(2.dp)
             .background(color = Color.White)
             .padding(10.dp)
+            )
 
-    ) {
-        val (imageRef, numOfColorRef, color1Ref, color2Ref, color3Ref, nameRef, addProductRef, priceRef) = createRefs()
+     {
+        val (imageRef, numOfColorRef, color1Ref, color2Ref, color3Ref, nameRef, addProductRef, supplierRetailPriceRef, priceRef) = createRefs()
         CoilImage(
             data = product.productInfo.imageUrl ?: "",
             contentDescription = "",
@@ -49,35 +54,48 @@ fun ProductListItem(product: Product) {
                 top.linkTo(imageRef.bottom, margin = 5.dp)
                 start.linkTo(parent.start)
             })
-        Circle(color = Color(0xFFE91E63), modifier = Modifier.constrainAs(color1Ref) {
+        Circle(color = Color(0xFF000000), modifier = Modifier.constrainAs(color1Ref) {
             top.linkTo(numOfColorRef.top)
             bottom.linkTo(numOfColorRef.bottom)
             start.linkTo(numOfColorRef.end, margin = 5.dp)
         })
-        Circle(color = Color(0xFF2196F3), modifier = Modifier.constrainAs(color2Ref) {
+        Circle(color = Color(0xFFFFFFFF), modifier = Modifier.constrainAs(color2Ref) {
             top.linkTo(numOfColorRef.top)
             bottom.linkTo(numOfColorRef.bottom)
             start.linkTo(color1Ref.end, margin = 5.dp)
         })
-        Circle(color = Color(0xFFFFEB3B), modifier = Modifier.constrainAs(color3Ref) {
+        Circle(color = Color(0xFF8D8D8D), modifier = Modifier.constrainAs(color3Ref) {
             top.linkTo(numOfColorRef.top)
             bottom.linkTo(numOfColorRef.bottom)
             start.linkTo(color2Ref.end, margin = 5.dp)
         })
         Text(
             text = product.productInfo.name,
-            color = Color(0xFFC96D50),
-            modifier = Modifier.constrainAs(nameRef) {
-                top.linkTo(numOfColorRef.bottom, margin = 5.dp)
+            color = Color(0xFF000000),
+            maxLines = 3,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier
+                .constrainAs(nameRef) {
+                    top.linkTo(numOfColorRef.bottom, margin = 5.dp)
+                    start.linkTo(parent.start)
+                    height=Dimension.value(52.dp)
+                })
+        Text(text = "${product.prices[0].sellPrice} vnđ",
+            color = Color(0xFFD60303),
+            fontSize = 17.sp,
+            modifier = Modifier.constrainAs(priceRef) {
+                top.linkTo(nameRef.bottom, margin = 5.dp)
                 start.linkTo(parent.start)
             })
         Text(
-            text = "${product.prices[0].sellPrice} vnđ",
-            color = Color(0xFFBB7A7A),
-            modifier = Modifier.constrainAs(priceRef) {
-                top.linkTo(nameRef.bottom, margin = 5.dp)
-                start.linkTo(parent.start, margin = 5.dp)
-            },
+            text = "${product.prices[0].supplierRetailPrice} vnđ",
+            color = Color(0xFF858585),
+            fontSize = 12.sp,
+            modifier = Modifier
+                .constrainAs(supplierRetailPriceRef) {
+                    top.linkTo(priceRef.bottom, margin = 5.dp)
+                    start.linkTo(parent.start, margin = 5.dp)
+                },
             textDecoration = TextDecoration.LineThrough
         )
 
@@ -92,7 +110,7 @@ fun ProductListItem(product: Product) {
                 .padding(2.dp)
                 .clip(shape = RoundedCornerShape(2.dp))
                 .constrainAs(addProductRef) {
-                    top.linkTo(priceRef.bottom, margin = 5.dp)
+                    top.linkTo(supplierRetailPriceRef.bottom, margin = 5.dp)
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
                     width = Dimension.fillToConstraints
@@ -113,4 +131,11 @@ fun Circle(color: Color, modifier: Modifier = Modifier) {
             .background(color)
 
     )
+
 }
+
+//@Preview
+//@Composable
+//fun DefaultPreView(){
+//
+//}
